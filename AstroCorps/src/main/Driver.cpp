a02eca84e31db8f30f72230bb9ef689b2fdb98d6
Matthew Dennerlein <matthew@technecure.com>
2019-07-31 00:25:46 -0400

    Added TinyXML2 for Data Serialization!

--- AstroCorps/src/main/Driver.cpp
+++ AstroCorps/src/main/Driver.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 
 #include "Application.h"
+#include "..\xml\tinyxml2.h"
+
+
+using namespace tinyxml2;
+#ifndef XMLCheckResult
+	#define XMLCheckResult(a_eResult) if (a_eResult != XML_SUCCESS) { printf("Error: %i\n", a_eResult); return a_eResult; }
+#endif
 
 int main() {
-	Application::start_application();
+	tinyxml2::XMLDocument xmlDoc;
+	tinyxml2::XMLError eResult = xmlDoc.LoadFile("SavedData.xml");
+	XMLCheckResult(eResult);
+
+	//Application::start_application();
 
 	std::cin.get();
 
