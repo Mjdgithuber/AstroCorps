1feea6c41bfd2a44ee8db13dc4ab41c0e50d74d5
Matthew Dennerlein <matthew@technecure.com>
2019-08-12 03:02:02 -0400

    Added Lua and Sol3 For Scripting!

--- AstroCorps/src/main/Driver.cpp
+++ AstroCorps/src/main/Driver.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 
-#include "Application.h"
+a#include "Application.h"
+#include "libs/sol/sol.hpp"
 
 int main() {
 	// start and run the application
 	Application::start_application(36, 3);
+
+	sol::state lua;
+
+	lua.open_libraries(sol::lib::base, sol::lib::package);
+	std::string a = lua.script(R"(
+				print('bark bark bark!')
+				print('hello emma')
+				print('femma')
+				return 'emma'
+	)");
+	std::cout << "Return \'" << a << "\'\n";
+
+	int x = lua.script("return 5");
+	std::cout << x << "\n";
 
 	std::cout << "\n\n***** Program Finished. Press Enter! *****\n";
 	std::cin.get();
